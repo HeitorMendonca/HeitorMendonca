4a5e2ca19043d9e471c27d585ac13bb2fa25b9e3
HeitorMendonca <120286501+HeitorMendonca@users.noreply.github.com>
2023-06-19 20:44:00 -0300

    AULA: 3 obstacles

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -2,6 +2,8 @@ import pygame
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 from dino_runner.components.dinosaur import Dinosaur
+from dino_runner.components.obstacles.obstacle_manage import ObstacleManager
+
 
 class Game:
     def __init__(self):
@@ -16,7 +18,7 @@ class Game:
         self.y_pos_bg = 380
 
         self.player = Dinosaur()
-
+        self.obstacle_manager = ObstacleManager()
 
     def run(self):
         # Game loop: events - update - draw
@@ -35,12 +37,14 @@ class Game:
     def update(self):
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
+        self.obstacle_manager.update(self)
 
     def draw(self):
         self.clock.tick(FPS)
-        self.screen.fill((255, 255, 255)) 
+        self.screen.fill((255, 255, 255)) # "#FFFFFF"
         self.draw_background()
         self.player.draw(self.screen)
+        self.obstacle_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
@@ -51,4 +55,4 @@ class Game:
         if self.x_pos_bg <= -image_width:
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
-        self.x_pos_bg -= self.game_speed
+        self.x_pos_bg -= self.game_speed    
